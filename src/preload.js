dceb37badec5bb3dc1b63e8ed70c8a4215a7eff3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-03 18:43:35 +0530

    src: preload

--- src/preload.js
+++ src/preload.js
@@ -8,6 +8,11 @@
  *
  */
 
+/**
+ * preload help us to connect ide to electron app
+ * This is like server/client relation
+ */
+
 const {
     contextBridge,
     ipcRenderer
@@ -16,7 +21,48 @@ const {
 contextBridge.exposeInMainWorld(
     "api", {
         send: (channel, data) => {
-            let validChannels = []
+            let validChannels = ["toMain",
+                'openWeb',
+                'open-file-dialog',
+                'open-project-direct',
+                'save-as-file-project',
+                'save-project',
+                'open-file-dialog-project',
+                'open-file-image',
+                'command',
+                'update-project',
+                'app-close',
+                'app-max',
+                'app-min',
+                'devtools',
+                'emulator',
+                'storage-get',
+                'storage-set',
+            ];
+            if (validChannels.includes(channel)) {
+                ipcRenderer.send(channel, data);
+            }
+        },
+
+        receive: (channel, func) => {
+            let validChannels = [
+                "fromMain",
+                'selected-file',
+                'saved-file',
+                'message',
+                'image-selected',
+                'terminal',
+                'terminal-error',
+                'build-success',
+                'emulator',
+                'emulator-terminal',
+                'storage-back',
+                'app-exit'
+            ];
+            
+            if (validChannels.includes(channel)) {
+                ipcRenderer.on(channel, (event, ...args) => func(...args));
+            }
         }
     }
 );
